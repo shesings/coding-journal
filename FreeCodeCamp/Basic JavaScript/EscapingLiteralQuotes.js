14fe7ee17ca4fac4f636851a2be64f52d0a81583
Sheetal <sheetalsingh7007@gmail.com>
2022-01-19 20:06:09 -0600

    quoting strings

---
+++ FreeCodeCamp/Basic JavaScript/EscapingLiteralQuotes.js
@@ -0,0 +1,8 @@
+// This is just to show that you can use the \" to use quotes in a string
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+// Note to self: single and double quotes work the same in JavaScript
+
+// 
+
