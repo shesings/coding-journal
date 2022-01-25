1fedccb5504cbe7e7df4531beb113d18126932d9
Sheetal <sheetalsingh7007@gmail.com>
2022-01-24 23:25:06 -0600

    concatenatingStrings

--- FreeCodeCamp/ConcatenatingStrings.js
+++ FreeCodeCamp/ConcatenatingStrings.js
@@ -3,3 +3,11 @@ const myStr = "This is the start. " + "This is the end.";
 let myStr = "This is the first sentence.";
 myStr += " This is the second sentence.";
 
+// Consturcting Strings with Variables
+const myName = "Sheetal";
+const myStr = "My name is " + myName + "and I am well!";
+
+// Appending Variables to Strings
+const someAdjective = "sad";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
