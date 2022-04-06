171e4be4aa2773635287f131283f465f65cd229f
Sheetal <sheetalsingh7007@gmail.com>
2022-04-06 02:29:24 -0500

    new updated code

--- FreeCodeCamp/Basic JavaScript/SwitchStatements.js
+++ FreeCodeCamp/Basic JavaScript/SwitchStatements.js
@@ -67,4 +67,94 @@ function sequentialSizes(val) {
   sequentialSizes(1);
 
   // Replacing If Else Chains with Switch
+  function chainToSwitch(val) {
+    let answer = "";
+  /*
+    if (val === "bob") {
+      answer = "Marley";
+    } else if (val === 42) {
+      answer = "The Answer";
+    } else if (val === 1) {
+      answer = "There is no #1";
+    } else if (val === 99) {
+      answer = "Missed me by this much!";
+    } else if (val === 7) {
+      answer = "Ate Nine";
+    }
+  */
+    switch(val) {
+      case "bob":
+        answer = "Marley";
+        break;
+      case 42:
+        answer = "The Answer";
+        break;
+      case 1:
+        answer = "There is no #1";
+        break;
+      case 99:
+        answer = "Missed me by this much!";
+        break;
+      case 7:
+        answer = "Ate Nine";
+        break;
+    }
+    return answer;
+  }
   
+  chainToSwitch(7);
+
+  // Returning Boolean Values from Functions
+  function isLess(a, b) {
+    // Only change code below this line
+    /*if (a < b) {
+      return true;
+    } else {
+      return false;
+    }*/
+    return a <= b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
+
+  // Return Early Pattern for Functions
+
+function abTest(a, b) {
+
+  if (a < 0 || b < 0) {
+    return undefined;
+  } 
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+
+// Counting Cards
+let count = 0;
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+}
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
